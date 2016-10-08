4daae46a11459e54f118c800fd2d93e1053a671c
Yeshe Wingerd <yeshito@gmail.com>
2016-10-08 15:52:00 -0700

    initial commit have most of landing page done

---
+++ script.js
@@ -0,0 +1,5 @@
+$( document ).ready(function () {
+  $(".button-collapse").sideNav();
+  $('.slider').slider({height: 300, transition: 500, interval: 2500, indicators: false});
+  $('.carousel').carousel({});
+})
