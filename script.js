8a6781d1afd85ca8f54fb2889b3fab0af55cd43e
Yeshe Wingerd <yeshito@gmail.com>
2016-10-08 22:23:53 -0700

    fixed apparent width problem, looks like it was a chrome issue

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $( document ).ready(function () {
   $(".button-collapse").sideNav();
   $('.slider').slider({height: 300, transition: 500, interval: 2500, indicators: false});
-  $('.carousel').carousel({});
+  $('.carousel').carousel();
 })
